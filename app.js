81f8a9cdf65b3fcdfdaf7f90bde063b441bf6180
priyangaanand <priyangaanand98@gmail.com>
2022-03-20 13:26:18 +0530

    Encryption

---
+++ app.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const ejs= require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+mongoose.connect('mongodb://localhost:27017/SecertsDB');
+const userSchema = new mongoose.Schema({
+    email:"String",
+    password:"String"
+});
+console.log(process.env.SECERT);
+userSchema.plugin(encrypt, { secret:process.env.SECERT,encryptedFields:["password"]});
+const userCollect = mongoose.model("user",userSchema);
+app.get("/",function(req,res)
+{
+ res.render("home");
+
+});
+app.get("/login",function(req,res)
+{
+res.render("login");
+});
+app.get("/register",function(req,res)
+{
+res.render("register");
+});
+app.post("/register",function(req,res)
+{
+    const userData = new userCollect({
+        email:req.body.username,
+        password:req.body.password
+    });
+    userData.save(function(err)
+    {
+        if(!err)
+        {
+            res.render("secrets");
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
+});
+app.post("/login",function(req,res)
+{
+    const username = req.body.username;
+    const password = req.body.password;
+    userCollect.findOne({email:username},function(err,founder)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            // if (founder)
+            {
+                if(founder.password==password)
+                {
+                     console.log(founder.password);
+                     console.log(password);
+                    res.render("secrets");
+                }
+            }
+           
+        }
+    })
+
+});
+app.listen(3000,function()
+{
+    console.log("Server is running on port 3000");
+});
